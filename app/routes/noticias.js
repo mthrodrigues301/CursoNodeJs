aaa6bdd9e45361905c8b18eba368a2285686ed28
Matheus Andrey Rodrigues <matheus.randrey@gmail.com>
2019-02-10 18:57:57 -0200

    primeira versao

---
+++ app/routes/noticias.js
@@ -0,0 +1,19 @@
+module.exports = function(app){
+
+    app.get('/noticias', function(req, res){
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'matheus',
+            password: '1234',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function(error, result){
+            res.send(result);
+        });
+        //res.render("noticias/noticias");
+    });
+}
