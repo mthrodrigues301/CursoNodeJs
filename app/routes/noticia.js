a32d2dae4fb496478fac93be8269ab9e374a4cee
Matheus Andrey Rodrigues <matheus.randrey@gmail.com>
2019-02-12 19:59:05 -0200

    Rotas

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,12 +1,12 @@
 module.exports = function(app){    
     
-    app.get('/noticias', function(req, res){
+    app.get('/noticia', function(req, res){
         
         var connection = app.config.dbConnection();
         var noticiasModel = app.app.models.noticiasmodel;
 
         noticiasModel.getNoticias(connection, function(error, result){
-            res.render("noticias/noticias", {noticias : result});        
+            res.render("noticias/noticia", {noticia : result});        
         });
     });
 }
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
 
-module.exports = function(){
+var connMySQL = module.exports = function(){
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'password',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function(){
+    return connMySQL;
 }
