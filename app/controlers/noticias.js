d1ed5c488efb4d5c256e6ab80b920bb953201b7d
Matheus Andrey Rodrigues <matheus.randrey@gmail.com>
2019-02-17 15:16:11 -0300

    Adicionando consulta por id dinamico

--- app/controlers/noticias.js
+++ app/controlers/noticias.js
@@ -10,8 +10,10 @@ module.exports.noticias = function(app, req, res){
 module.exports.noticia = function(app, req, res){
     var connection = app.config.dbConnection();
     var noticiasModel = new app.app.models.NoticiasDAO(connection);
+    
+    var id_noticia = req.query;
 
-    noticiasModel.getNoticias(function(error, result){
+    noticiasModel.getNoticia(id_noticia, function(error, result){
         res.render("noticias/noticia", {noticia : result});        
     });
 }
